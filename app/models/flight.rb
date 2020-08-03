e9b3c727be662a9fcc224b997d0dda3b125be79c
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-03 11:09:35 -0600

    Define flight#num_minors and #num_adults

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,12 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def num_minors
+    passengers.where('age < 18').count
+  end
+
+  def num_adults
+    passengers.where('age > 18').count
+  end
 end
