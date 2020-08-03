7e1ebd61dd0a73700d2d779abb39f84feceed321
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-03 09:15:43 -0600

    Outline feature tests

---
+++ spec/features/flights/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a flight's show page," do
+  it "I see all of that flight's information: number, date, time, departure city, and arrival city." do
+
+  end
+
+  it "I see the name of the airline this flight belongs to." do
+
+  end
+
+  it "I see the names of all passengers on this flight." do
+
+  end
+
+  it "I see the number of minors and adults on the flight." do
+
+  end
+end
---
+++ spec/features/passengers/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a passenger's show page," do
+  it "I see the passenger's name." do
+
+  end
+
+  it "I see a section displaying the numbers of all this passenger's flights." do
+
+  end
+
+  it "A flight number links to that flight's show page." do
+
+  end
+
+  it "I see a form to assign the passenger to a flight." do
+
+  end
+
+  it "When I fill in and submit the form with a flight number, I'm returned to the passenger's show page where I see the newly added flight number." do
+
+  end
+end
