a6aafad9523a3555252a834d82f59549536f571d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-03 09:22:55 -0600

    Create Passengers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803151611) do
+ActiveRecord::Schema.define(version: 20200803151916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,24 @@ ActiveRecord::Schema.define(version: 20200803151611) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.integer "number"
+    t.string "date"
+    t.string "time"
+    t.string "departure_city"
+    t.string "arrival_city"
+    t.bigint "airline_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "flights", "airlines"
 end
