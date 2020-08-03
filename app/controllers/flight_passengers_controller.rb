923106f6aea66e838cab5a92c92b99e9994aa008
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-03 10:53:56 -0600

    Define FlightPassenger #create action

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,3 +1,7 @@
 class FlightPassengersController < ApplicationController
-
+  def create
+    flight = Flight.find_by(number: params[:flight_number])
+    FlightPassenger.create(flight_id: flight.id, passenger_id: params[:passenger_id])
+    redirect_to "/passengers/#{params[:passenger_id]}"
+  end
 end
--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "When I visit a passenger's show page," do
     @passenger = Passenger.create(name: "Bob", age: 32)
     FlightPassenger.create(flight_id: @flight_1.id, passenger_id: @passenger.id)
     FlightPassenger.create(flight_id: @flight_2.id, passenger_id: @passenger.id)
-    FlightPassenger.create(flight_id: @flight_3.id, passenger_id: @passenger.id)
     visit "/passengers/#{@passenger.id}"
   end
 
@@ -17,21 +16,29 @@ RSpec.describe "When I visit a passenger's show page," do
     expect(page).to have_content(@passenger.name)
   end
 
-  it "I see a section displaying the numbers of all this passenger's flights, each of which are links to the respective flight show page." do
+  it "I see a section displaying the numbers of all this passenger's flights,
+  each of which are links to the respective flight show page." do
     within('.flights') do
       expect(page).to have_link(@flight_1.number)
       expect(page).to have_link(@flight_2.number)
-      expect(page).to have_link(@flight_3.number)
       click_on "#{@flight_1.number}"
     end
     expect(current_path).to eq("/flights/#{@flight_1.id}")
   end
 
-  it "I see a form to assign the passenger to a flight." do
-
-  end
-
-  it "When I fill in and submit the form with a flight number, I'm returned to the passenger's show page where I see the newly added flight number." do
-
+  it "I see a form to assign the passenger to a flight,
+  and when I fill in and submit the form with a flight number,
+  I'm returned to the passenger's show page where I see the newly added flight number." do
+    within('.flights') do
+      expect(page).to_not have_link(@flight_3.number)
+    end
+    within('.add_flight') do
+      fill_in :flight_number, with: @flight_3.number
+      click_on "Submit"
+    end
+    expect(current_path).to eq("/passengers/#{@passenger.id}")
+    within('.flights') do
+      expect(page).to have_link(@flight_3.number)
+    end
   end
 end
